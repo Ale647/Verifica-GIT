db2edb98a1eb31b6f8572831758a434738212b2b
Alessandro Bianchini <ALESSANDRO.BIANCHINI@local.ittcesena.it>
2025-11-21 08:37:09 +0100

    classe libro completata

--- verifica/verifica/Libro.cs
+++ verifica/verifica/Libro.cs
@@ -8,5 +8,46 @@ namespace verifica
 {
     internal class Libro
     {
+        public string titolo;
+        public string autore;
+        public int annoPubblicazione;
+        public string editore;
+        public int numeroPagine;
+        public Libro(string titolo, string autore, int annoPubblicazione, string editore, int numeroPagine)
+        {
+            this.titolo = titolo;
+            this.autore = autore;
+            this.annoPubblicazione = annoPubblicazione;
+            this.editore = editore;
+            this.numeroPagine = numeroPagine;
+        }
+
+        public override string ToString()
+        {
+            return $"{titolo}, {autore}, {annoPubblicazione}, {editore}, {numeroPagine} pagine";
+        }
+        public string ReadingTime()
+        {
+            string tempo;
+            if (numeroPagine < 100)
+            {
+                tempo = "1h";
+            }
+            else
+            {
+                if (numeroPagine >= 100 && numeroPagine < 200)
+                {
+                    tempo = "2h";
+                }
+                else
+                {
+                    tempo = ">2h";
+                }
+            }
+
+            return $"Tempo di lettura stimato: {tempo} ore";
+        }
+
     }
+
 }
